1468adff0f303a3d3f17d9cedf13f3e85ff8d57a
pc <pc@DESKTOP-HRA2IF8>
2020-07-03 17:45:19 +0900

    1

--- spring_projectE/src/main/java/com/koreait/projectE/contorller/pojectEController.java
+++ spring_projectE/src/main/java/com/koreait/projectE/contorller/pojectEController.java
@@ -1,9 +1,5 @@
 package com.koreait.projectE.contorller;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> branch 'master' of https://github.com/leemkyungmin/koreaitjavaweb0224.git
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.session.SqlSession;
@@ -12,37 +8,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-<<<<<<< HEAD
-import com.koreait.projectE.commom.Command;
-import com.koreait.projectE.commom.CustomerSignUpCommand;
-=======
 
 import com.koreait.projectE.command.boardViewCommand;
 import com.koreait.projectE.commom.Command;
-
->>>>>>> branch 'master' of https://github.com/leemkyungmin/koreaitjavaweb0224.git
-import com.koreait.projectE.dao.DAO;
+import com.koreait.projectE.commom.CustomerSignUpCommand;
 
 
 @Controller
 public class pojectEController {
 	
 	@Autowired
-<<<<<<< HEAD
 	private SqlSession sqlSession;
 	private Command command;
-=======
 
-	private SqlSession sqlsession;
-	
-	private Command command;
 
-	
-
->>>>>>> branch 'master' of https://github.com/leemkyungmin/koreaitjavaweb0224.git
-	
 	@RequestMapping("/")
 	public String goIndex() {
 		return "index";
@@ -54,7 +33,7 @@ public class pojectEController {
 		
 		model.addAttribute("request",request);
 		command= new boardViewCommand();
-		command.execute(sqlsession, model);
+		command.execute(sqlSession, model);
 		
 		return "board/viewPage";
 	}
