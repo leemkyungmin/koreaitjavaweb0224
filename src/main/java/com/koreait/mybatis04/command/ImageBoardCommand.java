43b93c4eee81b01efd8a3c2172f2aadd8a2fe4d4
kane9529 <kane9529@naver.com>
2020-07-03 09:12:25 +0900

    delete

--- src/main/java/com/koreait/mybatis04/command/ImageBoardCommand.java
+++
@@ -1,9 +0,0 @@
-package com.koreait.mybatis04.command;
-
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.ui.Model;
-
-public interface ImageBoardCommand {
-
-	public void execute(SqlSession sqlsession,Model model);
-}
