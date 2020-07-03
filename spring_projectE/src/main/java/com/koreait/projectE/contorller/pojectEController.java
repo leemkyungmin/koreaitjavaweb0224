5a14e42f59321b732f27a9f24a8a80abbb61fa0c
pc <pc@DESKTOP-HRA2IF8>
2020-07-03 15:20:50 +0900

    컨트롤러 추가

--- spring_projectE/src/main/java/com/koreait/projectE/contorller/pojectEController.java
+++ spring_projectE/src/main/java/com/koreait/projectE/contorller/pojectEController.java
@@ -10,4 +10,29 @@ public class pojectEController {
 	public String goIndex() {
 		return "index";
 	}
+	
+	@RequestMapping("loginChoicePage")
+	public String loginChoicePage() {
+		return "login/loginChoicePage";
+	}
+	
+	@RequestMapping("customerLoginPage")
+	public String goCustomerLoginPage() {
+		return "login/customerLoginPage";
+	}
+	@RequestMapping("deptLoginPage")
+	public String goDeptLoginPage() {
+		return "login/deptLoginPage";
+	}
+	
+	@RequestMapping("customerSignUp")
+	public String goSignUpPage() {
+		return "login/customerSignUpPage";
+	}
+	
+	@RequestMapping("deptSignUp")
+	public String godeptSingUpPage() {
+		return "login/deptSignUpPage";
+	}
+	
 }
