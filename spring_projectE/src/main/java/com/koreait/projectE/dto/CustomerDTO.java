e962fd21ddc0b355c28e2fa994c72259d9a65df3
kane9529 <kane9529@naver.com>
2020-07-03 10:46:53 +0900

    초기 설정

---
+++ spring_projectE/src/main/java/com/koreait/projectE/dto/CustomerDTO.java
@@ -0,0 +1,7 @@
+package com.koreait.projectE.dto;
+
+public class CustomerDTO {
+
+	private int cNo;
+	private String cId,cPw,cName,cNickname,cEmail,cDept;
+}
--- spring_projectE/src/main/java/com/koreait/projectE/dto/re.java
+++
@@ -1,5 +0,0 @@
-package com.koreait.projectE.dto;
-
-public class re {
-
-}
