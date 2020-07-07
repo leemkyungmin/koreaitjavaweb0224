9842cb97bb1840e74db9ef3f2179928ccd41cc44
pc <pc@E-26>
2020-07-07 15:54:46 +0900

    dfdf

--- spring_projectE/src/main/java/com/koreait/projectE/dao/ReviewDAO.java
+++ spring_projectE/src/main/java/com/koreait/projectE/dao/ReviewDAO.java
@@ -2,6 +2,6 @@ package com.koreait.projectE.dao;
 
 public interface ReviewDAO {
 
-	public void insertReview(String rTitle, String rContent, int rPoint,String rPoto);
+	public void insertReview(String rTitle, String rContent, int rPoint, String rPoto, int cNo, String dSaup_no);
 	
 }
