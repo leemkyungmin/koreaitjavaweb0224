e164d852844813429d0a6ab3269ac2e9e1c901ca
pc <pc@E-30>
2020-07-14 13:35:24 +0900

    123

--- spring_projectE/src/main/java/com/koreait/projectE/command/SearchCommand.java
+++ spring_projectE/src/main/java/com/koreait/projectE/command/SearchCommand.java
@@ -3,7 +3,9 @@ package com.koreait.projectE.command;
 import java.util.ArrayList;
 import java.util.Map;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.ui.Model;
@@ -19,11 +21,25 @@ public class SearchCommand implements Command {
 		
 		Map<String, Object> map = model.asMap();		
 		HttpServletRequest request = (HttpServletRequest) map.get("request");		
+		HttpServletResponse response = (HttpServletResponse) map.get("response");		
 		String query = request.getParameter("main-search");		
 		ListDAO lDAO = sqlSession.getMapper(ListDAO.class);		
 		ArrayList<DepartmentDTO> list = lDAO.search_list(query);		
 		model.addAttribute("list", list);
 		model.addAttribute("size", list.size());
+		
+		Cookie[] cookies = request.getCookies();
+		String addr="";
+		for(Cookie cook :cookies) {
+			if(cook.getName().equals("query")) {
+				System.out.println(cook.getValue());
+				addr =cook.getValue()+">"+query;
+			}
+		}
+		System.out.println(addr);
+		Cookie setCookie = new Cookie("query", addr);
+		setCookie.setMaxAge(60*60*24*7); 
+		response.addCookie(setCookie);
 
 	}
 
