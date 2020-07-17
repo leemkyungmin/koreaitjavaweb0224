04785683b9777aff308f969ec31bc0775b3b8593
kane9529 <kane9529@naver.com>
2020-07-17 11:53:01 +0900

    3333333

--- spring_projectE/src/main/java/com/koreait/projectE/contorller/BoardController.java
+++ spring_projectE/src/main/java/com/koreait/projectE/contorller/BoardController.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
@@ -39,7 +38,7 @@ import com.koreait.projectE.dto.ReviewDTO;
 
 
 
-@Controller
+@Controller	
 public class BoardController {
 	
 	@Autowired
